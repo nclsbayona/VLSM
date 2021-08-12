83f51a331ee1a1cd4c616ca872bef21b61dc0d6b
nclsbayona <nbayonaord@gmail.com>
2021-08-11 20:08:45 -0500

    ip check

--- vlsm_alg.py
+++ vlsm_alg.py
@@ -19,20 +19,34 @@ def input_data():
 
     #convert correct ip to int
     ip=list(map(int, ip))
+    ip=[ip, list(map(convert_decimal_to_binary_octet, ip))]
 
     mask=int(input("Net-Mask (Bits): "))
     while ((mask>32) or (mask<0)):
         mask=int(input("Not correct, try again. Net-Mask (Bits): "))
 
+    #Number of available hosts
+    available=2**(8-(mask%8))-2
+
     subnet_number=int(input("Number of subnets: "))
     subnets=list()
+    tot=0
     subnets.append(subnet_number)
     for i in range(subnet_number):
         name=input("Enter name of subnet "+str(i+1)+": ")
         num=int(input("Number of hosts of subnet '"+name+"': "))
-        subnets.append((name, num))
+        bits=ceil(log2(num+2))
+        tot+=(2**(bits))
+        subnets.append((name, num, bits))
+    subnets.append(tot)
 
-    return ip, mask, subnets
+    return ip, mask, available, subnets
+
+def vlsm(ip, mask, available, subnets):
+    if (available<subnets):
+        return False
+    
+    
 
 if (__name__=="__main__"):
     print (input_data())
