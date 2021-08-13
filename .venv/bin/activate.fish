6b744b5b3d785e5422d5a03a909ac7b0e4581e22
nclsbayona <nbayonaord@gmail.com>
2021-08-12 20:38:14 -0500

    half

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nclsbayona/Desktop/VLSM-CIDR-master/ENG/.venv"
+set -gx VIRTUAL_ENV "/home/nclsbayona/Desktop/VLSM/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- vlsm_alg.py
+++ vlsm_alg.py
@@ -1,5 +1,6 @@
 from prettytable import PrettyTable
 from math import log2, ceil
+from copy import deepcopy
 
 def binary_to_decimal(binary):
     binary=int(binary)
@@ -51,7 +52,7 @@ def input_data():
 
     return ip, mask, available, subnets
 
-def vlsm(ip, mask, available, subnets):
+def beautify(ip, mask, available, subnets):
     if (available<subnets[-1]):
         return False
     del subnets[-1]
@@ -75,17 +76,97 @@ def vlsm(ip, mask, available, subnets):
             if (octeto_bit>-1):
                 new_binary=new_binary[0:octeto_bit+1]
                 new_binary+=("0"*(8-len(new_binary)))
-                
+
         if (new_binary is not None):
             ip[1][len(ip)-octeto_mascara]=new_binary
             ip[0][len(ip)-octeto_mascara]=binary_to_decimal(new_binary)
             print ("New major address:", ip)
 
-    subnets=sorted(subnets)
-    return ip, subnets
+    elif (octeto_mascara>0):
+        new_binary=None
+        octet=ip[0][-(len(ip[0])-octeto_mascara)] #Last octet doesn't belong to mask completely
+
+        if (octet<(2**(8-octeto_bit)) and octet>0):
+            octeto_bit-=1
+            new_binary=ip[1][octeto_mascara] #Last octet doesn't belong completely to mask [BINARY]
+            if (octeto_bit>-1):
+                new_binary=new_binary[0:octeto_bit]
+                new_binary=(new_binary)+("0"*(8-len(new_binary)))
+            else:
+                new_binary="0"*8
+
+        elif (octet>(2**(8-octeto_bit))):
+            octeto_bit-=1
+            new_binary=ip[1][octeto_mascara] #Last octet doesn't belong completely to mask [BINARY]
+            if (octeto_bit>-1):
+                new_binary=new_binary[0:octeto_bit+1]
+                new_binary+=("0"*(8-len(new_binary)))
+
+        for (i) in range(octeto_mascara+1, len(ip[0])):
+            ip[0][i]=0
+            ip[1][i]=convert_decimal_to_binary_octet(0)
+
+        if (new_binary is not None):
+            ip[1][len(ip)-octeto_mascara]=new_binary
+            ip[0][len(ip)-octeto_mascara]=binary_to_decimal(new_binary)
+        print ("New major address:", ip)
+
+
+    subnets=sorted(subnets, key=lambda tup:tup[1], reverse=True)
+    return ip, mask, subnets
     
+def vlsm(ip, mask, subnets, table=None):
+    print ("\nIteracion\n")
+    table=table if table else list()
+    net=None
+    subnet=subnets[0]
+    name=subnet[0]
+    mask=(32-(subnet[2]))
+    print ("Aqui", subnets)
+    new_ip=ip.copy()
+    del subnets[0]
+    print ("0-Redes:",table)
+    if (len(table)>0):
+        new_ip=add_one_ip(new_ip, mask, table)
+        ip=new_ip.copy()
     
+    net=[name, new_ip, mask]
+
+    print ("1-Redes:",table)
+    table.append(net.copy())
+    print ("2-Redes:",table)
+
+    if (len(subnets)>0):
+        vlsm(ip.copy(), mask, subnets, table)
+    else:
+        print (table)
+        return table
+
+def add_one_ip(ip, mask, table):
+    dec_ip=ip[0]
+    bina_ip=ip[1]
+    table[-1][1]=[dec_ip, bina_ip]
+    print (table)
+    new_ip=deepcopy(ip)
+    bin_ip=new_ip[1]
+    octet=bin_ip[(mask//8)]
+    the_rest_before=octet[:((mask%8)-1)]
+    the_rest_after=octet[(mask%8):]
+    change=octet[(mask%8)-1]
+    if (change=='0'):
+        change='1'
+    print (the_rest_before, change, the_rest_after)
+    new_ip[0][mask//8], new_ip[1][(mask//8)]=binary_to_decimal(the_rest_before+change+the_rest_after), the_rest_before+change+the_rest_after
+    new_ip=[new_ip[0],new_ip[1]]
+    new_ip=new_ip.copy()
+    print (table)
+    print ("Voy a retornar la ip cambiada:",new_ip)
+    return (deepcopy(new_ip))
 
 if (__name__=="__main__"):
     ip, mask, available, subnets=input_data()
-    print (vlsm(ip, mask, available, subnets))
+    try:
+        ip, mask, subnets=beautify(ip, mask, available, subnets)
+    except Exception:
+        print ("Can't")
+    print (vlsm(ip, mask, subnets))
